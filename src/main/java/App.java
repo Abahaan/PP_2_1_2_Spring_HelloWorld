e07cf1f0f62f71dcc18777a1e5a4d9b2dfdb0cdc
abahaan.kokov@gmail.com <pOj65xT69>
2022-08-09 15:00:15 +0700

    Spring 2.1 @Scope, @Bean and appContext

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.HierarchicalMessageSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
@@ -8,5 +9,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Сравнение по ссылке beanов Helloworld = " + (bean == bean2));
+
+        Cat beanCat =
+                applicationContext.getBean(Cat.class);
+        Cat beanCat2 =
+                applicationContext.getBean(Cat.class);
+        System.out.println("Сравнение по ссылке бинов CAT = " + (beanCat == beanCat2));
     }
 }
